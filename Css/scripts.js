fc5735c9bfb34c44cbb29ab8ca5254bd2981aeaa
Sharjeel-Iftikhar <isharjeel63@gmail.com>
2021-10-29 04:10:35 +0500

    Css,html pages added

---
+++ Css/scripts.js
@@ -0,0 +1,50 @@
+let img = document.querySelector('#img1');
+let img2 = document.querySelector('#img2');
+let img3 = document.querySelector('#img3');
+let img4 = document.querySelector('#img4');
+let img5 = document.querySelector('#img5');
+let img6 = document.querySelector('#img6');
+let img7 = document.querySelector('#img7');
+let img8 = document.querySelector('#img8');
+let img9 = document.querySelector('#img9');
+let btt1 = document.querySelector('#btt1');
+let btt2 = document.querySelector('#btt2');
+let btt3 = document.querySelector('#btt3');
+
+btn1.addEventListener('click', ()=>{
+    img.src = 'Images/Bikes/honda.jpeg';
+    img2.src = 'Images/Bikes/bmw.png';
+    img3.src = 'Images/Bikes/Yamaha.png';
+    img4.src = 'Images/Bikes/super.png';
+    img5.src = 'Images/Bikes/royal.jpg';
+    img6.src = 'Images/Bikes/harley.jpg';
+    img7.src = 'Images/Bikes/Ducati.jpg';
+    img8.src = 'Images/Bikes/Suzuki.jpg';
+    img9.src = 'Images/Bikes/united.jpg';
+})
+
+btn2.addEventListener('click', ()=>{
+    img.src = 'Images/Trucks/chervolt.jpg';
+    img2.src = 'Images/Trucks/ford.jpg';
+    img3.src = 'Images/Trucks/honda.jpg';
+    img4.src = 'Images/Trucks/jac.jpg';
+    img5.src = 'Images/Trucks/jeep.jpg';
+    img6.src = 'Images/Trucks/gmc.jpeg';
+    img7.src = 'Images/Trucks/hino.png';
+    img8.src = 'Images/Trucks/ram.png';
+    img9.src = 'Images/Trucks/isuzu.jpg';
+
+})
+
+btn3.addEventListener('click', ()=>{
+    img.src = 'Images/honda.jpg';
+    img2.src = 'Images/icons8-kia-144.png';
+    img3.src = 'Images/MG.jpg';
+    img4.src = 'Images/icons8-suzuki-144.png';
+    img5.src = 'Images/hyundai.jpg';
+    img6.src = 'Images/nissan.jpg';
+    img7.src = 'Images/faw-logo.jpg';
+    img8.src = 'Images/toyota.jpg';
+    img9.src = 'Images/proton.jpg';
+})
+
