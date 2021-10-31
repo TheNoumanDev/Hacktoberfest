cbdda053dc0b2498bb777573a4808c8fe650fb58
katsuNakajima <40666782+katsuNakajima@users.noreply.github.com>
2021-10-31 20:33:56 +0900

    added lcm algo in python

---
+++ Python Contribution/lcm.py
@@ -0,0 +1,23 @@
+#Name: Katsuaki Nakajima
+
+#Username: katsuNakajima
+
+#Favorite Language: Rust
+
+
+import math
+
+
+def lcm(a: int, b: int) -> int:
+    y = a*b / math.gcd(a, b)
+    return int(y)
+
+
+def main():
+    x = lcm(8, 12)
+
+    print(x)
+
+
+if __name__ == '__main__':
+    main()
