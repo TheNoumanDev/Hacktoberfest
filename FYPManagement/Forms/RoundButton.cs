ee0480697b12f7cbc61c578cefc016a60805fa6c
Umair-Manzoor-47 <2020CS24@student.uet.edu.pk>
2022-10-01 21:00:20 +0500

    ATM Management system along with database

---
+++ FYPManagement/Forms/RoundButton.cs
@@ -0,0 +1,42 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Drawing.Drawing2D;
+using System.Windows.Forms;
+using System.Linq;
+using System.Text;
+
+namespace FYPManagement.Forms
+{
+    public class RoundButton : Button
+    {
+        protected override void OnPaint(System.Windows.Forms.PaintEventArgs e)
+        {
+            GraphicsPath grPath = new GraphicsPath();
+            grPath.AddEllipse(0, 0, ClientSize.Width, ClientSize.Height);
+            this.Region = new System.Drawing.Region(grPath);
+            base.OnPaint(e);
+        }
+    }
+    public class RoundPictureBox : PictureBox
+    {
+        protected override void OnPaint(System.Windows.Forms.PaintEventArgs e)
+        {
+            GraphicsPath grPath = new GraphicsPath();
+            grPath.AddEllipse(0, 0, ClientSize.Width, ClientSize.Height);
+            this.Region = new System.Drawing.Region(grPath);
+            base.OnPaint(e);
+        }
+    }
+    public class RoundPanel : Panel
+    {
+        protected override void OnPaint(System.Windows.Forms.PaintEventArgs e)
+        {
+            GraphicsPath grPath = new GraphicsPath();
+            grPath.AddEllipse(0, 0, ClientSize.Width, ClientSize.Height);
+            this.Region = new System.Drawing.Region(grPath);
+            base.OnPaint(e);
+        }
+    }
+}
+
