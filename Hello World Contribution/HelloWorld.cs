5e0e8c37ea4df5931f7db38308f2c070d5e8d64e
iqrasarwar <71145709+iqrasarwar@users.noreply.github.com>
2022-10-04 23:57:25 +0500

    Hello Worl in c#

---
+++ Hello World Contribution/HelloWorld.cs
@@ -0,0 +1,15 @@
+// Name: Iqra Sarwar
+// Username: iqrasarwar
+// Favorite Language: C#
+
+using System;
+namespace HelloWorld
+{
+   class Program
+   {
+      static void Main(string[] args)
+      {
+         Console.WriteLine("Hello World!");
+      }
+   }
+}
