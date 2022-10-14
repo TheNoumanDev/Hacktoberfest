2312d9a0ecc919fa0dec9fef9f08891730158f84
pagepublishernobi <87115580+pagepublishernobi@users.noreply.github.com>
2022-10-15 00:55:48 +0530

    Create helloWorld.c

---
+++ Hello World Contribution/helloWorld.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main() {
+   // printf() displays the string inside quotation
+   printf("Hello, World!");
+  //Never Forgot to use Semicolon ; at the end of statement
+   return 0;
+}
