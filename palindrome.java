b32cbd518aa392c4e45e34648594e1c0b6d2727e
CodeMaster26 <116353580+CodeMaster26@users.noreply.github.com>
2022-10-22 09:26:15 +0530

    Create palindrome.java

---
+++ palindrome.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+class Palindrome
+{  
+    public static void main(String args[])
+    {  
+       int r,sum=0,temp,n;    
+       System.out.println ("Please enter a number :"); 
+        Scanner sc= new Scanner (System.in);
+        n= sc.nextInt();
+       temp=n;    
+      while(n>0)
+      {    
+        r=n%10;  //getting remainder  
+        sum=(sum*10)+r;    
+        n=n/10;    
+     }    
+       if(temp==sum)    
+         System.out.println("palindrome number ");    
+       else    
+         System.out.println("not palindrome");    
+   }  
+}  
