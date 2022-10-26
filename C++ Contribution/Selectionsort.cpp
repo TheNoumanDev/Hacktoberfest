38c7eac34117e72831409cceaacd9558ba727a9d
TheNoumanDev <m_nouman@hotmail.com>
2022-10-26 22:54:25 +0500

    updated

--- C++ Contribution/Selectionsort.cpp
+++ C++ Contribution/Selectionsort.cpp
@@ -1,38 +1,44 @@
-#include<iostream>
+// Selection sort in C++
+
+#include <iostream>
 using namespace std;
-//selection sort
-//find the min element in unsorted array
-//and swap it with element at begining
 
-int main()
-{
-    int n;
-    cout<<"Enter the number of elements of an array : ";
-    cin>>n;
-    int arr[n];
-    cout<<"Enter the elements of an array :";
-    for(int i=0;i<n;i++)
-    {
-        cin>>arr[i];
-    }
-    for (int i=0;i<n;i++)
-    {
-        for(int j=i+1;j<n;j++)
-        {
-           if(arr[j]<arr[i])
-           {
-              int temp=arr[i];
-              arr[i] = arr[j];
-              arr[j] = temp ;
+// function to swap the the position of two elements
+void swap(int *a, int *b) {
+  int temp = *a;
+  *a = *b;
+  *b = temp;
+}
 
-           } 
-           
-        }
+// function to print an array
+void printArray(int array[], int size) {
+  for (int i = 0; i < size; i++) {
+    cout << array[i] << " ";
+  }
+  cout << endl;
+}
+
+void selectionSort(int array[], int size) {
+  for (int step = 0; step < size - 1; step++) {
+    int min_idx = step;
+    for (int i = step + 1; i < size; i++) {
+
+      // To sort in descending order, change > to < in this line.
+      // Select the minimum element in each loop.
+      if (array[i] < array[min_idx])
+        min_idx = i;
     }
-    cout<<"our sorted array is : ";
-    for (int i = 0; i<n ; i++)
-    {
-        cout<<arr[i]<<" ";
-    }
-    return 0;
+
+    // put min at the correct position
+    swap(&array[min_idx], &array[step]);
+  }
+}
+
+// driver code
+int main() {
+  int data[] = {20, 12, 10, 15, 2};
+  int size = sizeof(data) / sizeof(data[0]);
+  selectionSort(data, size);
+  cout << "Sorted array in Acsending Order:\n";
+  printArray(data, size);
 }
---
+++ Java Contribution/PythonOption.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class home2{
+ 
+   public static void main(String[] args) {
+      int n;
+      Scanner scanner = new Scanner(System.in);
+ System.out.print("What is outside:");
+n=scanner.nextInt();
+if(n==1){
+  System.out.print("Take Rest");
+}
+else{
+  System.out.print("play");
+   }
+   }
+}
